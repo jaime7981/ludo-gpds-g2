292ada6334123744bc2885518d9c358bbeedafc6
jaime7981 <jmfernandez1@miaundes.cl>
2023-09-04 21:29:16 -0300

    piece class and player init function

---
+++ classes/piece.py
@@ -0,0 +1,12 @@
+
+
+class Piece():
+    def __init__(self, piece_number = -1, x = -1, y  = -1) -> None:
+        self.piece_number = piece_number
+        self.position = [x, y]
+
+    def __str__(self) -> str:
+        return str(self.piece_number)
+    
+    def __repr__(self) -> str:
+        return str(self.piece_number)
--- classes/player.py
+++ classes/player.py
@@ -1,5 +1,21 @@
+from piece import Piece
 
 class Player():
-    def __init__(self) -> None:
-        pass
+    def __init__(self, name = 'default') -> None:
+        self.name = name
+        self.on_board_pieces = []
+        self.off_board_pieces = []
+        
+        self.assign_pieces()
+
+    def assign_pieces(self):
+        for i in range(4):
+            self.off_board_pieces.append(Piece(i + 1))
+
+    def __str__(self) -> str:
+        return self.name
+
+    def __repr__(self) -> str:
+        return self.name
+
     
